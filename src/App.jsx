5dc38da272f16499bd9f4d6eb9caf2ca37f60200
pallavi300 <pallavi81@ievitableinfotech.com>
2024-06-05 15:19:46 +0530

    setup the todo app

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Navbar from "./Componnet/NavBar"
+
+function App() {
+
+  return (
+    <>
+      <Navbar />
+     <div className="container mx-auto">
+      <div className="bg-red-600">
+        <h1>Your Todo List</h1>
+      </div>
+     </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/Componnet/NavBar.jsx
@@ -0,0 +1,10 @@
+import React from "react" 
+
+const Navbar = () => {
+    return (
+        <div>
+            Hey I Am NavBar
+        </div>
+    )
+}
+export default Navbar
