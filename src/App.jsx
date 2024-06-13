a6e13e15b17eb18a9efcb0a67d4ce503193e3cd3
pallavi300 <pallavi81@ievitableinfotech.com>
2024-06-13 15:06:36 +0530

    created todo add

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,60 @@
-import Navbar from "./Componnet/NavBar"
+import { useState } from "react";
+import Navbar from "./Componnet/NavBar";
+import { v4 as uuidv4 } from "uuid";
 
 function App() {
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+
+
+  const handleAdd = () => {
+    setTodos([...todos, { id: uuidv4(), todo, isCompleted: false }]);
+    setTodo("");
+    console.log(todos)
+  }
+  const handleEdit = () => {
+
+  }
+  const handleDelete = () => {
+
+  }
+  const handleChange = (e) => {
+    setTodo(e.target.value)
+  }
+  const handleCheckbox = (e) => {
+    let id = e.target.name;
+  }
+
 
   return (
     <>
       <Navbar />
-     <div className="container mx-auto">
-      <div className="bg-red-600">
-        <h1>Your Todo List</h1>
+      <div className="container mx-auto my-5 rounded-xl p-5 bg-violet-100 min-h-[80vh] ">
+        <div className="addTodo my-5 ">
+          <h2 className="text-lg font-bold"> Add a Todo</h2>
+          <input onChange={handleChange} value={todo.id} type="text" className="w-1/2" />
+          <button onClick={handleAdd} className="bg-violet-800 hover:bg-violet-950 p-2 py-1 text-sm font-bold text-white rounded-md mx-6">Add</button>
+        </div>
+        <h2 className="text-lg font-bold">Your Todo </h2>
+        <div className="todos">
+          {todos.map(item => {
+            return (
+              <div className="todo flex w-1/4 justify-between my-3" key={item}>
+                <input name={item.id} onChange={handleCheckbox} type="checkbox" value={item.isCompleted} />
+                <div className={item.isCompleted ? "line-through" : ""}>{item.todo}</div>
+                <div className="buttons">
+                  <button onClick={() => handleEdit(item.id)} className="bg-violet-800 hover:bg-violet-950 p-2 py-1 text-sm font-bold text-white rounded-md mx-1">Edit</button>
+                  <button onClick={() => handleDelete(item.id)} className="bg-violet-800 hover:bg-violet-950 p-2 py-1 text-sm font-bold text-white rounded-md mx-1">Delete</button>
+                </div>
+              </div>
+            );
+          })}
+
+        </div>
       </div>
-     </div>
     </>
   )
 }
 
 export default App
+
--- src/Componnet/NavBar.jsx
+++ src/Componnet/NavBar.jsx
@@ -1,10 +1,15 @@
-import React from "react" 
 
 const Navbar = () => {
     return (
-        <div>
-            Hey I Am NavBar
-        </div>
+        <nav className="flex  justify-between bg-indigo-900 text-white py-2">
+            <div className="logo">
+                <span className="font-bold text-xl mx-9 ">iTask</span>
+            </div>
+            <ul className="flex gap-8 mx-9">
+                <li className="cursor-pointer hover:font-bold transition-all">Home</li>
+                <li className="cursor-pointer hover:font-bold transition-all ">You Tasks</li>
+            </ul>
+        </nav>
     )
 }
 export default Navbar
